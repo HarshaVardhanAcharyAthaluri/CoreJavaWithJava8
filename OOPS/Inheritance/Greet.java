ea72e985e4ebd91b205e821eda73383a9c7e1def
HarshaVardhan <athaluri.harsha@gmail.com>
2022-09-29 08:42:21 +0530

    inheritance

---
+++ OOPS/Inheritance/Greet.java
@@ -0,0 +1,7 @@
+final class Greet{
+	int number = 100;
+	
+	void display(){
+		System.out.println("Hello There! Iam executing from Greet");
+	}
+}
---
+++ OOPS/Inheritance/myapp.java
@@ -0,0 +1,10 @@
+class myapp {
+
+	public static void main(String[] args){
+	
+		Greet g = new Greet();
+		
+		g.display();
+	
+	}
+}
